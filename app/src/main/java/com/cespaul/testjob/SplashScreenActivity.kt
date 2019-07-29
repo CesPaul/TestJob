0314e6266511d0fc7ce25604d3186d2f68aa928a
CesPaul PC <scrin333@gmail.com>
2019-07-30 04:28:03 +0700

    Создан временный сплэш скрин

---
+++ app/src/main/java/com/cespaul/testjob/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.cespaul.testjob
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val intent = Intent(
+            applicationContext,
+            MainActivity::class.java
+        )
+        startActivity(intent)
+        finish()
+    }
+}
